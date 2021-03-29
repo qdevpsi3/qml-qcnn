987a523d6b6e200cd91d98b3c5ce292a64769ae8
qdevpsi3 <45946838+qdevpsi3@users.noreply.github.com>
2021-03-29 15:52:54 +0200

    update convolution layer

--- src/qcnn/quantum_convolution.py
+++ src/qcnn/quantum_convolution.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import distributions
 
 
@@ -30,6 +29,7 @@ class QuantumConv2d(nn.Conv2d):
             padding (int, optional): convolution padding. Defaults to 0.
             dilation (int, optional): convolution dilation. Defaults to 1.
         """
+        # convolution layer
         super(QuantumConv2d, self).__init__(in_channels,
                                             out_channels,
                                             kernel_size,
@@ -40,20 +40,23 @@ class QuantumConv2d(nn.Conv2d):
                                             bias=False,
                                             padding_mode='zeros')
 
+        # unfold operation
+        self.unfold = nn.Unfold(kernel_size, dilation, padding, stride)
+
         # quantum parameters
         self.quantum_eps = quantum_eps
         self.quantum_ratio = quantum_ratio
         self.quantum_cap = quantum_cap
 
         # add quantum gradient estimation error to the kernel
-        def get_quantum_gradient(grad):
-            noise = quantum_delta * torch.randn(grad.shape, device=grad.device)
+        def simulate_quantum_gradient(grad):
+            noise = torch.randn(grad.shape, device=grad.device)
             grad_norm = torch.norm(grad)
-            error = noise * grad_norm
+            error = quantum_delta * grad_norm * noise
             grad += error
             return grad
 
-        self.weight.register_hook(get_quantum_gradient)
+        self.weight.register_hook(simulate_quantum_gradient)
 
     def extra_repr(self):
         s = '{in_channels}, {out_channels}, kernel_size={kernel_size}, '
@@ -75,46 +78,41 @@ class QuantumConv2d(nn.Conv2d):
         output = torch.clamp(output, 0., self.quantum_cap)
 
         # get quantum output
-        quantum_output = self.get_quantum_output(input, output)
+        quantum_output = self.simulate_quantum_output(input, output)
 
         # update convolutional layer output
         output.data = quantum_output.data
 
         return output
 
-    def get_quantum_output(self, input, output):
+    def simulate_quantum_output(self, input, output):
         with torch.no_grad():
             # get kernel norm
-            kernel_matrix = self.weight.data.flatten(start_dim=1).transpose(
-                0, 1)
-            kernel_norm = torch.norm(kernel_matrix,
-                                     dim=0).repeat(input.size(0), 1, 1)
+            kernel_matrix = self.weight.data.flatten(start_dim=1)
+            kernel_matrix = kernel_matrix.transpose(0, 1)
+            kernel_norm = torch.norm(kernel_matrix, dim=0)
+            kernel_norm = kernel_norm.repeat(input.size(0), 1, 1)
 
             # get input norm
-            input_matrix = torch.nn.functional.unfold(
-                input,
-                self.kernel_size,
-                stride=self.stride,
-                padding=self.padding,
-                dilation=self.dilation).transpose(1, 2)
-            input_norm = torch.norm(input_matrix, dim=2).unsqueeze(2)
+            input_matrix = self.unfold(input)
+            input_matrix = input_matrix.transpose(1, 2)
+            input_norm = torch.norm(input_matrix, dim=2)
+            input_norm = input_norm.unsqueeze(2)
 
             # add gaussian noise
-            vars = torch.bmm(input_norm, kernel_norm).reshape(output.shape)
-            noise = self.quantum_eps * torch.randn(output.shape,
-                                                   device=vars.device)
-            errors = 2 * torch.mul(vars, noise)
-            output = output + errors
+            product_norm = torch.bmm(input_norm, kernel_norm)
+            product_norm = product_norm.reshape(output.shape)
+            noise = torch.randn(output.shape, device=output.device)
+            output += 2 * self.quantum_eps * product_norm * noise
             output = torch.clamp(output, 0., self.quantum_cap)
 
             # quantum sampling
             num_samples = int(self.quantum_ratio * output.shape[1:].numel())
             probs = output.flatten(start_dim=1)
-            distribution = torch.distributions.Categorical(probs=probs)
+            distribution = distributions.Categorical(probs=probs)
             samples = distribution.sample((num_samples, )).flatten()
-            idxs = torch.arange(0,
-                                input.size(0)).repeat(1,
-                                                      num_samples).flatten()
+            idxs = torch.arange(0, input.size(0))
+            idxs = idxs.repeat(1, num_samples).flatten()
             mask = torch.zeros_like(probs)
             mask[idxs, samples] = 1.
             mask = mask.reshape(output.shape)
