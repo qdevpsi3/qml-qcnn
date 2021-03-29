a5d393d1d7fd9beca445f7a6ab467ff4f7bf574c
qdevpsi3 <45946838+qdevpsi3@users.noreply.github.com>
2021-03-29 22:43:05 +0200

    update conv layer

--- src/qcnn/quantum_convolution.py
+++ src/qcnn/quantum_convolution.py
@@ -40,29 +40,15 @@ class QuantumConv2d(nn.Conv2d):
                                             bias=False,
                                             padding_mode='zeros')
 
+        # set/check quantum parameters
+        self.set_quantum_params(quantum_eps, quantum_cap, quantum_ratio,
+                                quantum_delta)
+
         # unfold operation
         self.unfold = nn.Unfold(kernel_size, dilation, padding, stride)
 
-        # quantum parameters
-        self.quantum_eps = quantum_eps
-        self.quantum_ratio = quantum_ratio
-        self.quantum_cap = quantum_cap
-        self.quantum_delta = quantum_delta
-
+        # gradient operation
         self.weight.register_hook(self.simulate_quantum_gradient)
-
-    def extra_repr(self):
-        s = '{in_channels}, {out_channels}, kernel_size={kernel_size}, '
-        s += 'quantum_eps={quantum_eps}, quantum_cap = {quantum_cap}, '
-        s += 'quantum_ratio={quantum_ratio}, quantum_delta = {quantum_delta}, '
-        s += 'stride={stride}'
-        if self.padding != (0, ) * len(self.padding):
-            s += ', padding={padding}'
-        if self.dilation != (1, ) * len(self.dilation):
-            s += ', dilation={dilation}'
-        if self.bias is None:
-            s += ', bias=False'
-        return s.format(**self.__dict__)
 
     def forward(self, input):
 
@@ -80,42 +66,90 @@ class QuantumConv2d(nn.Conv2d):
 
     def simulate_quantum_output(self, input, output):
         with torch.no_grad():
-            # get kernel norm
-            kernel_matrix = self.weight.data.flatten(start_dim=1)
-            kernel_matrix = kernel_matrix.transpose(0, 1)
-            kernel_norm = torch.norm(kernel_matrix, dim=0)
-            kernel_norm = kernel_norm.repeat(input.size(0), 1, 1)
+            if self.quantum_eps > 0.:
+                # get kernel norm
+                kernel_matrix = self.weight.data.flatten(start_dim=1)
+                kernel_matrix = kernel_matrix.transpose(0, 1)
+                kernel_norm = torch.norm(kernel_matrix, dim=0)
+                kernel_norm = kernel_norm.repeat(input.size(0), 1, 1)
 
-            # get input norm
-            input_matrix = self.unfold(input)
-            input_matrix = input_matrix.transpose(1, 2)
-            input_norm = torch.norm(input_matrix, dim=2)
-            input_norm = input_norm.unsqueeze(2)
+                # get input norm
+                input_matrix = self.unfold(input)
+                input_matrix = input_matrix.transpose(1, 2)
+                input_norm = torch.norm(input_matrix, dim=2)
+                input_norm = input_norm.unsqueeze(2)
 
-            # add gaussian noise
-            product_norm = torch.bmm(input_norm, kernel_norm)
-            product_norm = product_norm.reshape(output.shape)
-            noise = torch.randn(output.shape, device=output.device)
-            output += 2 * self.quantum_eps * product_norm * noise
-            output = torch.clamp(output, 0., self.quantum_cap)
+                # add gaussian noise
+                product_norm = torch.bmm(input_norm, kernel_norm)
+                product_norm = product_norm.reshape(output.shape)
+                noise = torch.randn(output.shape, device=output.device)
+                output += 2 * self.quantum_eps * product_norm * noise
+                output = torch.clamp(output, 0., self.quantum_cap)
 
-            # quantum sampling
-            num_samples = int(self.quantum_ratio * output.shape[1:].numel())
-            probs = output.flatten(start_dim=1)
-            distribution = distributions.Categorical(probs=probs)
-            samples = distribution.sample((num_samples, )).flatten()
-            idxs = torch.arange(0, input.size(0))
-            idxs = idxs.repeat(1, num_samples).flatten()
-            mask = torch.zeros_like(probs)
-            mask[idxs, samples] = 1.
-            mask = mask.reshape(output.shape)
-            output = mask * output
+            if self.quantum_ratio < 1.:
+                # quantum sampling
+                num_samples = int(self.quantum_ratio *
+                                  output.shape[1:].numel())
+                probs = output.flatten(start_dim=1)
+                distribution = distributions.Categorical(probs=probs)
+                samples = distribution.sample((num_samples, )).flatten()
+                idxs = torch.arange(0, input.size(0))
+                idxs = idxs.repeat(1, num_samples).flatten()
+                mask = torch.zeros_like(probs)
+                mask[idxs, samples] = 1.
+                mask = mask.reshape(output.shape)
+                output = mask * output
         return output
 
     def simulate_quantum_gradient(self, grad):
-        # add quantum gradient estimation error to the kernel
-        noise = torch.randn(grad.shape, device=grad.device)
-        grad_norm = torch.norm(grad)
-        error = self.quantum_delta * grad_norm * noise
-        grad += error
+        if self.quantum_delta > 0.:
+            # add quantum gradient estimation error to the kernel
+            noise = torch.randn(grad.shape, device=grad.device)
+            grad_norm = torch.norm(grad)
+            error = self.quantum_delta * grad_norm * noise
+            grad += error
         return grad
+
+    def set_quantum_params(self,
+                           quantum_eps=None,
+                           quantum_cap=None,
+                           quantum_ratio=None,
+                           quantum_delta=None):
+        if quantum_eps is not None:
+            assert 0. <= quantum_eps <= 1., 'epsilon should verify: 0.<=eps<=1.'
+            self.quantum_eps = quantum_eps
+        if quantum_cap is not None:
+            assert 0. < quantum_cap, 'cap should verify: 0.<cap'
+            self.quantum_cap = quantum_cap
+        if quantum_ratio is not None:
+            assert 0. < quantum_ratio <= 1., 'ratio should verify: 0.<ratio<=1.'
+            self.quantum_ratio = quantum_ratio
+        if quantum_delta is not None:
+            assert 0 <= quantum_delta <= 1., 'delta should verify: 0.<=delta<=1.'
+            self.quantum_delta = quantum_delta
+
+    def convert_to_classical(self):
+        layer = nn.Conv2d(self.in_channels,
+                          self.out_channels,
+                          self.kernel_size,
+                          self.stride,
+                          self.padding,
+                          self.dilation,
+                          groups=1,
+                          bias=False,
+                          padding_mode='zeros')
+        layer.weight.data = self.weight.data
+        return layer
+
+    def extra_repr(self):
+        s = '{in_channels}, {out_channels}, kernel_size={kernel_size}, '
+        s += 'quantum_eps={quantum_eps}, quantum_cap = {quantum_cap}, '
+        s += 'quantum_ratio={quantum_ratio}, quantum_delta = {quantum_delta}, '
+        s += 'stride={stride}'
+        if self.padding != (0, ) * len(self.padding):
+            s += ', padding={padding}'
+        if self.dilation != (1, ) * len(self.dilation):
+            s += ', dilation={dilation}'
+        if self.bias is None:
+            s += ', bias=False'
+        return s.format(**self.__dict__)
